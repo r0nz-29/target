4ccd87d1a5a14cd57850e07caeb831e631c4e46d
r0nz-29 <gs2019048@sgsitsindore.in>
2023-07-21 15:41:10 +0530

    feat: multiplayer

--- src/components/ResultsModal/index.jsx
+++ src/components/ResultsModal/index.jsx
@@ -1,22 +1,46 @@
 // eslint-disable-next-line react/prop-types
 import {useGlobalState} from "../../store/index.js";
 import {calculateWPM} from "../../utils/index.js";
+import {VictoryChart, VictoryLine, VictoryScatter, VictoryTheme} from "victory";
 
-export default function ResultsModal() {
+export default function ResultsModal({duration}) {
 	const {
 		errors,
 		typedParagraph,
+		graph,
+		errorPoints
 	} = useGlobalState();
 
-	const wpm = calculateWPM(typedParagraph, errors, 15);
+	const wpm = calculateWPM(typedParagraph, errors, duration);
 	const accuracy = 100 * ((typedParagraph.length - errors) / typedParagraph.length)
+	console.log(graph);
+	console.log(errorPoints);
 	return (
 		<div className="bg-black/[0.6] absolute w-screen h-screen flex justify-center items-center">
-			<div className="p-4 bg-white w-1/4 rounded-lg shadow dark:bg-gray-700">
-				<p>Accuracy: <span className="text-2xl">{accuracy.toFixed(2)}%</span></p>
+			<div className="p-4 bg-white w-1/3 rounded-lg shadow dark:bg-gray-700">
+				{/*<VictoryChart*/}
+				{/*	theme={VictoryTheme.material}*/}
+				{/*>*/}
+				{/*	<VictoryLine*/}
+				{/*		style={{*/}
+				{/*			data: {stroke: "#3f83f8"},*/}
+				{/*			parent: {border: "1px solid #ccc"}*/}
+				{/*		}}*/}
+				{/*		data={graph}*/}
+				{/*		interpolation="natural" label="chappal"/>*/}
+				{/*	<VictoryScatter*/}
+				{/*		style={{data: {fill: "#f00"}}}*/}
+				{/*		size={3}*/}
+				{/*		data={errorPoints}*/}
+				{/*		symbol='triangleDown'*/}
+				{/*	/>*/}
+				{/*</VictoryChart>*/}
+				<p>Accuracy: <span className="text-2xl">{accuracy > 0 ? accuracy.toFixed(2) : 0}%</span></p>
 				<p>Words per minute: <span className="text-2xl">{wpm.toFixed(2)}</span></p>
 				<br/>
-				<button className="px-6 py-2 bg-blue-500 rounded-lg text-white" onClick={() => window.location.reload()}>Restart</button>
+				<button className="px-6 py-2 bg-blue-500 rounded-lg text-white"
+								onClick={() => window.location.reload()}>Restart
+				</button>
 			</div>
 		</div>
 	)
