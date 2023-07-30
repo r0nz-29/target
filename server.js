76e341c1e6bcd37f586544fe428ef476ed42f5c2
r0nz-29 <gs2019048@sgsitsindore.in>
2023-07-30 11:24:24 +0530

    feat: finalize

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const { config } = require("dotenv");
 const mongoose = require("mongoose");
 const { join_lobby } = require("./lobbies.js");
 const { disconnect } = require("process");
+const { signin, signup } = require("./controller/user.js");
+const { saveUser, getUserHistory} = require("./controller/user_history.js")
+const {saveUserHistory} = require("./controller/user_history");
 config();
 const TIME=50;
 const Lobbies = {"easy":[],"medium":[],"hard":[]};
 const Socket_list=new Map();
 const Running = new Map();
-const { signin, signup } = require("./controller/user.js");
-const { saveUser, getUserHistory} = require("./controller/user_history.js")
-const {saveUserHistory} = require("./controller/user_history");
 const Private_lobbies=new Map();
 const app = express();
 const structure = {
@@ -29,6 +29,13 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send(
+      '<img src="https://res.cloudinary.com/daa4wqa2h/image/upload/v1689840737/afj_jhrf75.png" alt="pogger" />'
+    );
+});
 app.post("/signup",signup);
 app.post("/signin",signin);
 app.post("/user",saveUser);
@@ -50,42 +57,42 @@ const io = require("socket.io")(http, {
 
 function dis(socket_id){
   let a=Socket_list.get(socket_id);
-    if(Running.get(a)){
-      Running.get(a).delete(socket_id);
-    }
+  if(Running.get(a)){
+    Running.get(a).delete(socket_id);
+  }
   if(a){
-  io.sockets.in(a).emit('leave',socket_id);
+    io.sockets.in(a).emit('leave',socket_id);
   }
 }
 
 
 function transfer_lobbies(difficulty){
-let a=[];
-let l=Lobbies[difficulty];
+  let a=[];
+  let l=Lobbies[difficulty];
 
-for (let i = 0; i < l.length; i++) {
-  l[i].time--;
+  for (let i = 0; i < l.length; i++) {
+    l[i].time--;
 
-  if (l[i].time === 0) {
-    const mp = new Map();
+    if (l[i].time === 0) {
+      const mp = new Map();
 
-    // console.log(l[i].participants.length);
-    for (let x = 0; x < l[i].participants.length; x++) {
-      // Socket_list.set(l[i].participants[x],l[i].lobbie_id);
-      mp.set(l[i].participants[x], { speed: 0, pos: 0, over: false,accuracy:0,errors:0});
+      // console.log(l[i].participants.length);
+      for (let x = 0; x < l[i].participants.length; x++) {
+        // Socket_list.set(l[i].participants[x],l[i].lobbie_id);
+        mp.set(l[i].participants[x], { speed: 0, pos: 0, over: false,accuracy:0,errors:0 });
+      }
+      Running.set(l[i].lobbie_id, mp);
+      io.sockets.in(l[i].lobbie_id).emit('start');
+    } else {
+      a.push(l[i]);
     }
-  Running.set(l[i].lobbie_id, mp);
-  io.sockets.in(l[i].lobbie_id).emit('start');
-  } else {
-    a.push(l[i]);
   }
-}
-return a;
+  return a;
 }
 // Deletes Running lobbies with 0 players
 setInterval(function (){
   // console.log(Running);
-	for (const [key, value] of Running) {
+  for (const [key, value] of Running) {
 
     if(value.size==0){
       Running.delete(key);
@@ -120,24 +127,21 @@ mongoose
   });
 
 io.on("connection", function (socket) {
-
   console.log(socket.id);
-  socket.on("join", function (obj) {
-    const user=obj.username;
-    const difficulty=obj.difficulty;
+  socket.on("join", function (difficulty) {
     console.log('diff --- ' + difficulty);
     if(difficulty.length<10){
-    const room_id = join_lobby(Lobbies, difficulty, socket);
-    socket.join(room_id);
-    Socket_list.set(socket.id,room_id);
-    let x=0;
-    for(let i=0;i<Lobbies[difficulty].length;i++){
-      if(Lobbies[difficulty].lobbie_id===room_id){
-        x=i;
+      const room_id = join_lobby(Lobbies, difficulty, socket);
+      socket.join(room_id);
+      Socket_list.set(socket.id,room_id);
+      let x=0;
+      for(let i=0;i<Lobbies[difficulty].length;i++){
+        if(Lobbies[difficulty].lobbie_id===room_id){
+          x=i;
+        }
       }
-    }
-    console.log('lobby' + Lobbies[difficulty][x]);
-    io.sockets.in(room_id).emit("new_member",{"data":Lobbies[difficulty][x],"user":user,"socket":socket.id});
+      console.log('lobby' + Lobbies[difficulty][x]);
+      io.sockets.in(room_id).emit("new_member",Lobbies[difficulty][x]);
     }
     else{
 
@@ -147,13 +151,13 @@ io.on("connection", function (socket) {
     console.log(socket.id);
     dis(socket.id);
   })
-function calculate_wpm(pos,accuracy,errors){
-  const minutes = TIME/ 60;
-	const wordsTyped = pos / 5;
-	const wrongWordsTyped = errors / 5;
-	const wpm = (wordsTyped - wrongWordsTyped) / minutes;
-	return wpm > 0 ? wpm : 0;
-}
+  function calculate_wpm(pos,accuracy,errors){
+    const minutes = TIME/ 60;
+    const wordsTyped = pos / 5;
+    const wrongWordsTyped = errors / 5;
+    const wpm = (wordsTyped - wrongWordsTyped) / minutes;
+    return wpm > 0 ? wpm : 0;
+  }
   socket.on("new_wpm",function (data){
     const room_id=Socket_list.get(socket.id);
     Running.get(room_id).set(socket.id,data);
@@ -174,8 +178,8 @@ function calculate_wpm(pos,accuracy,errors){
       console.log("udaaa")
     }
     else{
-    console.log(res);
-    io.sockets.in(room_id).emit("update", res);
+      console.log(res);
+      io.sockets.in(room_id).emit("update", res);
     }
   })
   // socket.on("leave")
